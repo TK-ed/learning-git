8fd31966fee5f4b02883330a05bd505ebdadb118
TK-ed <tharunstark11@gmail.com>
2023-08-01 11:02:02 +0530

    change path parameter

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def user():
-    return "name"
+
+
+# @app.get('/{name}')
+# def user(name: str):
+#     return name
+@app.get('/{name}')
+def user(name: str):
+    return name
+
 
 if __name__=="__main__":
     uvicorn.run("main:app",reload=True)
